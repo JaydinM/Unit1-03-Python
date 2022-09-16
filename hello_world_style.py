8eb7e4d11a9abef47616e40ea40d9d2ab3bfb2e5
Jaydin Madore <jaydin.madore@stu.ocsb.ca>
2022-09-16 17:58:46 +0000

    Add heelo_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By Jaydin Madore
+# Date: 2022-09-16 
+# This program displays Hello, World! with proper style
+
+
+def main():
+    print("Hello, World! with Proper style")
+
+
+if __name__ == "__main__":
+    main()
